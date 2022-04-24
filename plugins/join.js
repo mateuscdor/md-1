961002719bcb06bc8d7ce03fb024983fa2aeaeeb
mistery4 <80912288+mistery4@users.noreply.github.com>
2022-04-25 05:18:17 +0700

    Update join.js

--- plugins/join.js
+++ plugins/join.js
@@ -1,18 +1,21 @@
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
 
 let handler = async (m, { conn, text }) => {
-try {
     let [_, code] = text.match(linkRegex) || []
-    if (!code) throw 'Link invalid'
+    if (!code) throw 'Link Salah'
     let res = await conn.groupAcceptInvite(code)
-    m.reply(`Berhasil join ke grupmu`)
-  } catch (e) {
-    console.log(e)
-}
+    conn.reply(m.chat, `Berhasil join ke grup ${await conn.getName(res)}`, m).then(() => {
+        var jumlahHari = 86400000 * 0.5
+        var now = new Date() * 1
+        if (now < global.db.data.chats[m.chat].expired) global.db.data.chats[m.chat].expired += jumlahHari
+        else global.db.data.chats[m.chat].expired = now + jumlahHari
+        conn.reply(res, `*${conn.user.name}* adalah bot whatsapp, diundang oleh @${m.sender.split`@`[0]}\n${conn.user.name} akan keluar otomatis setelah: ${await conn.msToDate(global.db.data.chats[res].expired - now)}`.trim(), null, { mentions: [m.sender], contextInfo: { mentionedJid: [m.sender] } })
+    })
 }
 handler.help = ['join <chat.whatsapp.com>']
 handler.tags = ['main']
 
 handler.command = /^join$/i
 
+handler.limit = true
 module.exports = handler
