e5a060bb815368ff23c84c2dd00bd3d44f5c6680
mistery4 <80912288+mistery4@users.noreply.github.com>
2022-04-25 05:34:31 +0700

    Update gm-tekateki_ans.js

--- plugins/gm-tekateki_ans.js
+++ plugins/gm-tekateki_ans.js
@@ -5,17 +5,17 @@ handler.before = async function (m) {
     let id = m.chat
     if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*tete/i.test(m.quoted.contentText)) return !0
     this.tekateki = this.tekateki ? this.tekateki : {}
-    if (!(id in this.tekateki)) return m.reply('Soal itu telah berakhir')
+    if (!(id in this.tekateki)) return this.sendButton(m.chat, 'Soal itu telah berakhir', wm, 'Lagi?', '.tekateki', m)
     if (m.quoted.id == this.tekateki[id][0].id) {
         let json = JSON.parse(JSON.stringify(this.tekateki[id][1]))
         if (['.tete', 'Bantuan', ''].includes(m.text)) return !0
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
             global.db.data.users[m.sender].exp += this.tekateki[id][2]
-            await this.sendB(m.chat, `Selamat *@${m.sender.split(`@`)[0]}* jawaban kamu benar, bonus +${this.tekateki[id][2]} XP`, wm, null, [['Teka Teki', '.tekateki']], m, {mentions: [m.sender]})
+            await this.sendButton(m.chat, benar + ` +${this.tekateki[id][2]} XP`, wm, 'Lagi?', '.tekateki', m)
             clearTimeout(this.tekateki[id][3])
             delete this.tekateki[id]
-        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
-        else m.reply(`*Salah!*`)
+        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) this.sendButton(m.chat, dikit, wm, 'Bantuan', '.tete', m)
+        else this.sendButton(m.chat, salah, wm, 'Bantuan', '.tete', m)
     }
     return !0
 }
