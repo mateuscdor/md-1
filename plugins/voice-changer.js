c87bd7441a3b64040c92923bf7748d86df079265
mistery4 <80912288+mistery4@users.noreply.github.com>
2022-04-25 06:28:00 +0700

    Update voice-changer.js

--- plugins/voice-changer.js
+++ plugins/voice-changer.js
@@ -1,5 +1,4 @@
-const { unlinkSync, readFileSync } = require('fs')
-const { join } = require('path')
+const { fs } = require('fs')
 const { exec } = require('child_process')
 
 let handler = async (m, { conn, args, __dirname, usedPrefix, command }) => {
@@ -7,7 +6,7 @@ let handler = async (m, { conn, args, __dirname, usedPrefix, command }) => {
         let q = m.quoted ? m.quoted : m
         let mime = ((m.quoted ? m.quoted : m.msg).mimetype || '')
         let set
-        if (/bass/.test(command)) set = '-af equalizer=f=94:width_type=o:width=2:g=30'
+        if (/bass/.test(command)) set = '-af equalizer=f=54:width_type=o:width=2:g=20'
         if (/blown/.test(command)) set = '-af acrusher=.1:1:64:0:log'
         if (/deep/.test(command)) set = '-af atempo=4/4,asetrate=44500*2/3'
         if (/earrape/.test(command)) set = '-af volume=12'
@@ -18,22 +17,18 @@ let handler = async (m, { conn, args, __dirname, usedPrefix, command }) => {
         if (/robot/.test(command)) set = '-filter_complex "afftfilt=real=\'hypot(re,im)*sin(0)\':imag=\'hypot(re,im)*cos(0)\':win_size=512:overlap=0.75"'
         if (/slow/.test(command)) set = '-filter:a "atempo=0.7,asetrate=44100"'
         if (/smooth/.test(command)) set = '-filter:v "minterpolate=\'mi_mode=mci:mc_mode=aobmc:vsbmc=1:fps=120\'"'
-        if (/tupai|squirrel|chipmunk/.test(command)) set = '-filter:a "atempo=0.5,asetrate=65100"'
+        if (/tupai/.test(command)) set = '-filter:a "atempo=0.5,asetrate=65100"'
         if (/audio/.test(mime)) {
-            let ran = getRandom('.mp3')
-            let filename = join(__dirname, '../tmp/' + ran)
-            let media = await q.download(true)
-            exec(`ffmpeg -i ${media} ${set} ${filename}`, async (err, stderr, stdout) => {
-                await unlinkSync(media)
-                if (err) throw `_*Error!*_`
-                let buff = await readFileSync(filename)
-                conn.sendFile(m.chat, buff, ran, null, m, true, {
-                type: 'audioMessage', // paksa tanpa convert di ffmpeg
-                ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
-                })
-                //conn.sendFile(m.chat, buff, ran, null, m, /vn/.test(args[0]), { quoted: m, mimetype: 'audio/mp4' })
-                //await unlinkSync(filename)
-            })
+        m.reply(wait)
+        let media = await conn.downloadAndSaveMediaMessage(q)
+        let ran = getRandom('.mp3')
+        exec(`ffmpeg -i ${media} ${set} ${ran}`, (err, stderr, stdout) => {
+        fs.unlinkSync(media)
+        if (err) return m.reply(err)
+        let buff = fs.readFileSync(ran)
+        conn.sendMessage(m.chat, { audio: buff, mimetype: 'audio/mpeg' }, { quoted : m })
+        fs.unlinkSync(ran)
+        })
         } else throw `Balas vn/audio dengan perintah *${usedPrefix + command}*`
     } catch (e) {
         throw e
