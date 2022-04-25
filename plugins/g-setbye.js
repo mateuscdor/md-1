741d75f1cc4a3d7bacc82709e52826b7197e2097
mistery4 <80912288+mistery4@users.noreply.github.com>
2022-04-25 17:32:07 +0700

    Update g-setbye.js

--- plugins/g-setbye.js
+++ plugins/g-setbye.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
   if (text) {
-    if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
+    if (!(isAdmin || isOwner)) return global.dfail('admin', m, conn)
     db.data.chats[m.chat].sBye = text
     m.reply('Bye berhasil diatur\n@user (Mention)')
   } else throw `Penggunaan:\n${usedPrefix + command} <teks>\n\ncontoh:\n${usedPrefix + command} byebye @user`
