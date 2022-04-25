deca71944a80805d0c23da9a9edff97634cd9b30
mistery4 <80912288+mistery4@users.noreply.github.com>
2022-04-25 17:31:17 +0700

    Update g-setwelcome.js

--- plugins/g-setwelcome.js
+++ plugins/g-setwelcome.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
   if (text) {
-    if (!(isAdmin || isOwner)) return dfail('admin', m, conn)
+    if (!(isAdmin || isOwner)) return global.dfail('admin', m, conn)
     db.data.chats[m.chat].sWelcome = text
     m.reply('Welcome berhasil diatur\n@user (Mention)\n@subject (Judul Grup)\n@desc (Deskripsi Grup)')
   } else throw `Penggunaan:\n${usedPrefix + command} <teks>\n\nContoh:\n${usedPrefix + command} selamat datang @user digrup @subject\n\n@desc`
