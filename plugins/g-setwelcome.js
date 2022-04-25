27ce1d7d9fa81eec3845f7f90ef3e7037a515427
mistery4 <80912288+mistery4@users.noreply.github.com>
2022-04-25 17:40:32 +0700

    Update g-setwelcome.js

--- plugins/g-setwelcome.js
+++ plugins/g-setwelcome.js
@@ -1,12 +1,14 @@
-let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
+let handler = async (m, { conn, text, isROwner, isOwner, isAdmin, usedPrefix, command }) => {
   if (text) {
-    if (!(isAdmin || isOwner)) return global.dfail('admin', m, conn)
-    db.data.chats[m.chat].sWelcome = text
+    if (isROwner) global.conn.welcome = text
+    else if ((isOwner || isAdmin)) conn.welcome = text
+    global.db.data.chats[m.chat].sWelcome = text
     m.reply('Welcome berhasil diatur\n@user (Mention)\n@subject (Judul Grup)\n@desc (Deskripsi Grup)')
-  } else throw `Penggunaan:\n${usedPrefix + command} <teks>\n\nContoh:\n${usedPrefix + command} selamat datang @user digrup @subject\n\n@desc`
+  } else throw `Teksnya mana?\n\ncontoh:\n${usedPrefix + command} hai, @user!\nSelamat datang di grup @subject\n\n@desc`
 }
 handler.help = ['setwelcome <teks>']
 handler.tags = ['group']
-handler.command = /^setwelcome$/i
+handler.command = /^(setwelcome|setw)$/i
+handler.group = true
 
 module.exports = handler
